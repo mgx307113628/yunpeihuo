be908357e3b463f17c4fa8a47bb1333eed3d7106
shrike <revolution@aliyun.com>
2017-10-17 17:51:12 +0800

    config

--- yphapp/__init__.py
+++ yphapp/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import os
+import config
 
 app = Flask(__name__)
-app.config.from_object('config')
 
+#配置
+cfg = config.CONFIG_CLASS[os.getenv('YPH_FLASK_CONFIG_MOD') or 'development']
+app.config.from_object(cfg)
+cfg.init_app(app)
+#额外配置
+app.config.from_envvar('YPH_FLASK_CONFIG_EX', True)
+
+#注册views
 from . import views
