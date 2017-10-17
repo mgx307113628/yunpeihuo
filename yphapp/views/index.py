678a0cc640caf6f7d570e215ad00fb3366909eaf
karas <revolution@aliyun.com>
2017-10-17 22:47:07 +0800

    json

--- yphapp/views/index.py
+++ yphapp/views/index.py
@@ -1,5 +1,10 @@
+from flask import json, request, jsonify
 from .. import app
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    data = request.get_data()
+    print("%s", str(data))
+    if data:
+        json.loads(data)
+    return jsonify(code='Hello World!')
