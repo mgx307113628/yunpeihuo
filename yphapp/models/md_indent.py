8ccf8c63cdfcead77709cf48eb50f9301c89f6f1
shrike <revolution@aliyun.com>
2017-10-23 16:48:08 +0800

    order

--- yphapp/models/md_indent.py
+++ yphapp/models/md_indent.py
@@ -13,3 +13,20 @@ class Indent(db.Model):
 
     def __repr__(self):
         return '<Indent %s>'%str(self.id)
+
+
+class IndentNum(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    process = db.Column(db.Integer)
+    thread = db.Column(db.Integer)
+    degree = db.Column(db.Integer)
+    index = db.Column(db.Integer)
+
+    def __init__(self, process, thread, degree, index):
+        self.process = process
+        self.thread = thread
+        self.degree = degree
+        self.index = index
+
+    def __repr__(self):
+        return '<IndentNum %d-%d>'%(self.process, self.thread)
--- yphapp/views/order.py
+++ yphapp/views/order.py
@@ -4,42 +4,85 @@ from .. import app, db
 from ..models import md_indent
 from werkzeug.local import Local
 import time
+import random
 
 bp_order = Blueprint('order', __name__) 
 
-order_cnt = Local()
+order_id_pool = Local()
+step_size = 10000
 
 
 def gen_order_id():
     """
-    :return:订单号.最大值为18446744073709551615
-            订单号存于MYSQL的BIGINT
-            BIGINT无符号数范围：0~18446744073709551615
+    :return: 订单号
+    订单号格式(共11位):
+    1位业务类型 + 1位进程ID + 1位线程ID + 2位degree + 4位序列数字 + 2位随机数字
+    degree+序列数字 取值为0到999999,共一百万个
+
+    由于订单号格式,有以下限制:
+    1. 必须使用uwsgi开启应用
+    2. work进程数量不得超过10个
+    3. 每个work进程的线程数量不得超过10个
+    4. 订单号最多数量为1000000个, 可通过增加degree位数增加订单号数量
+
+    订单号存于indent表,为MYSQL的BIGINT类型,一定要注意不要溢出
+    BIGINT无符号数范围：0 ~ 18446744073709551615
+    BIGINT有符号数范围：-9223372036854775808 ~ 9223372036854775807
     """
     tname = threading.current_thread().name
     if type(tname) is bytes:
         tname = str(tname, encoding='utf8')
-    #用uwsgi启动,最多线程数为10个: uWSGIWorker1Core0
+    #用uwsgi启动,每个进程最多线程数为10个: uWSGIWorker1Core0
     if tname[:11] == 'uWSGIWorker1Core0' and tname[12:16] == 'Core':
-        tid = tname[11] + str(int(tname[16])+1)
-    #测试:用python启动
+        process = int(tname[11])
+        thread = int(tname[16])
+        if process >= 10 or thread >= 10:
+            raise RuntimeError()
+    #用python启动单进程(用于开发和测试)
     elif tname == 'MainThread':
-        tid = '00'
+        process = 0
+        thread = 0
     elif tname[:7] == 'Thread-':
-        tid = '0%s'%tname[7]
+        process = 0
+        thread = int(tname[7])
     else:
         raise RuntimeError()
 
-    now = int(time.time() * 1000) % 1000000000000 #只能使用约31年,31年后可能会出现订单号重复
-
-    tm, num = getattr(order_cnt, 'same_time_count', [0,0])
-    if now == tm:
-        num += 1
+    refresh = False
+    indent_num = md_indent.IndentNum.query.filter_by(process=process, thread=thread).first()
+    if indent_num is None:
+        degree = index = 0
+        indent_num = md_indent.IndentNum(process, thread, degree, index)
+        db.session.add(indent_num)
+        refresh = True
     else:
-        num = 1
-    setattr(order_cnt, 'same_time_count', [now,num])
+        indent_num.index += 1
+        if indent_num.index >= step_size:
+            indent_num.degree += 1
+            indent_num.index = 0
+            refresh = True
+        degree = indent_num.degree
+        index = indent_num.index
+    db.session.commit()
 
-    return int('1%s%d%.2d'%(tid, now, num)) #17位
+    sequence = getattr(order_id_pool, 'sequence', None)
+    if sequence is None or refresh:
+        sequence = []
+        lst = range(step_size)
+        crtidx = len(lst)-1
+        random.seed(71634+degree) #保证每一阶段,服务器重启后生成的号码序列都一样
+        while crtidx >= 0:
+            i = random.randint(0, crtidx)
+            sequence.append(lst[i])
+            lst[i], lst[crtidx]= lst[crtidx], lst[i]
+            crtidx -= 1
+        random.seed(int(time.time()))
+    setattr(order_id_pool, 'sequence', None)
+    num = sequence[index]
+
+    rd = random.randint(0, 99)
+
+    return int('1%d%d%.2d%.4d%.2d'%(process, thread, degree, num, rd)) #共11位
 
 @bp_order.route('/new')
 def order_new():
